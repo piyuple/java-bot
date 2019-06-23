654661e054b55a570afb9c869c3333968b918a7a
Piyush Ranjan <piyuple@users.noreply.github.com>
2019-06-23 08:56:24 +0530

    connected to Slack app

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 var Botkit = require('botkit');
 require('dotenv').config();
 
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFICATION_TOKEN) {
+    console.log('Error: Specify CLIENT_ID, CLIENT_SECRET, VERIFICATION_TOKEN and PORT in environment');
+    process.exit(1);
+} else {
+    console.log('Good job, you have the variables!');
+}
+
+var controller = Botkit.slackbot({
+    json_file_store: './db_slackbutton_slash_command/',
+    debug: true,
+    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET
+});
+
+controller.configureSlackApp({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+    scopes: ['commands', 'bot']
+});
+
+var bot = controller.spawn({
+    token: process.env.BOT_TOKEN,
+    incoming_webhook: {
+        url: 'WE_WILL_GET_TO_THIS'
+    }
+}).startRTM();
