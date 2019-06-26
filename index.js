3b96b4ae328d1741a32774300d65165f4afcb71a
Piyush Ranjan <piyuple@users.noreply.github.com>
2019-06-26 09:54:25 +0530

    testing bug #2

--- index.js
+++ index.js
@@ -86,14 +86,15 @@ function getSub(valid, bot, message) {
     console.log("querying external API with token");
     request(options.uri + "/" + token, function (err, res, bdy) {
             console.log("response received");
+            console.log("status:" + result.status.id);
             if (!err && res.statusCode == 200) {
                 result = JSON.parse(bdy);
-
+                console.log("status:" + result.status.id);
                 if (valid) {
                     if(result.status.id < 3)
                         setTimeout(getSub, 2000, true, bot, message);
                     else if (result.status.id > 3)
-                        bot.reply(message, '*status:* ' + result.status.description + ((result.stdout) ? '\n*output:* ' + result.stdout : '') + ((result.time) ? '\n*time:* ' + result.time + 's' : '') + ((result.memory) ? '\n*memory:* ' + result.memory + 'kB' : '') + ((result.stderr) ? '\n*stderr:* \n' + result.stderr : '') + ((result.compile_output) ? '\n*Compile output:* \n' + result.compile_output : '') + ((result.message) ? '\n*Message:* ' + result.message : ''));
+                        bot.reply(message, '*status:* ' + result.status.description + (result.stdout ? '\n*output:* ' + result.stdout : '') + (result.time ? '\n*time:* ' + result.time + 's' : '') + (result.memory ? '\n*memory:* ' + result.memory + 'kB' : '') + (result.stderr ? '\n*stderr:* \n' + result.stderr : '') + (result.compile_output ? '\n*Compile output:* \n' + result.compile_output : '') + (result.message ? '\n*Message:* ' + result.message : ''));
                 }
                 else
                     console.log("body:\n" + bdy);
